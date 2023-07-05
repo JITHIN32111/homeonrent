5d5d689fcd1d720792a1820e9494da92449cf9c8
Jithin C <jith97972@gmail.com>
2023-07-05 21:23:08 +0530

    baseurl

--- frontend/src/config/axiosConfig.js
+++ frontend/src/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+const baseURL=process.env.NODE_ENV==="production"?'/api':import.meta.env.VITE_BACKEND_URL
 const axiosConfig = axios.create({
-  baseURL:import.meta.env.VITE_BACKEND_URL,
+  baseURL,
   withCredentials: true,   
 });
 
